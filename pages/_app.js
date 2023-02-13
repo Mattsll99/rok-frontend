3124f81fa4700e33bee03556bad6ede5403827a1
Mattsll99 <matthiassylla21@gmail.com>
2023-02-13 09:48:39 +0100

    init

---
+++ pages/_app.js
@@ -0,0 +1,53 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+//import type { AppProps } from "next/app";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  goerli,
+  polygonMumbai,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const alchemyKey = "w7eEPiIoWugKjoU7vCuJhmfwAihhPNqQ";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: alchemyKey,
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "ROK",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
